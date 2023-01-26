7416eb59cdae0348d6b556da96eaeb0a31caf934
Timilehin Bakare <timmybaks2002@gmail.com>
2023-01-26 10:05:59 +0100

    Set up MUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button'
 
 const App = () => {
   return (
     <div>
       hello world!
+      <Button variant="contained">HI</Button>
     </div>
   )
 }
